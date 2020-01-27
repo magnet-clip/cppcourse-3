05ace2e5b6fbef317f3e8ab39fbb7852b1397eaa
Rustam Guseynov <guseynov.r.v@gmail.com>
2020-01-27 22:41:05 +0300

    Updated test framework

--- test_runner.cpp
+++
@@ -1,9 +0,0 @@
-#include "test_runner.h"
-#include <string>
-
-TestRunner::~TestRunner() {
-  if (fail_count > 0) {
-    cerr << fail_count << " unit tests failed. Terminate" << endl;
-    exit(1);
-  }
-}
--- test_runner.h
+++ test_runner.h
@@ -1,33 +1,20 @@
 #pragma once
 
-#include <exception>
 #include <iostream>
 #include <map>
 #include <set>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-// template <typename T> bool operator==(const set<T> &a, const set<T> &b) {
-//   if (a.size() != b.size()) {
-//     return false;
-//   }
-
-//   for (const auto &item : a) {
-//     if (!b.count(item)) {
-//       return false;
-//     }
-//   }
-
-//   return true;
-// }
-
-template <class T> ostream &operator<<(ostream &os, const set<T> &s) {
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& s) {
   os << "{";
   bool first = true;
-  for (const auto &x : s) {
+  for (const auto& x : s) {
     if (!first) {
       os << ", ";
     }
@@ -37,24 +24,25 @@ template <class T> ostream &operator<<(ostream &os, const set<T> &s) {
   return os << "}";
 }
 
-template <class T> ostream &operator<<(ostream &os, const vector<T> &s) {
-  os << "[";
+template <class T>
+ostream& operator<<(ostream& os, const set<T>& s) {
+  os << "{";
   bool first = true;
-  for (const auto &x : s) {
+  for (const auto& x : s) {
     if (!first) {
       os << ", ";
     }
     first = false;
     os << x;
   }
-  return os << "]";
+  return os << "}";
 }
 
 template <class K, class V>
-ostream &operator<<(ostream &os, const map<K, V> &m) {
+ostream& operator<<(ostream& os, const map<K, V>& m) {
   os << "{";
   bool first = true;
-  for (const auto &kv : m) {
+  for (const auto& kv : m) {
     if (!first) {
       os << ", ";
     }
@@ -65,31 +53,58 @@ ostream &operator<<(ostream &os, const map<K, V> &m) {
 }
 
 template <class T, class U>
-void AssertEqual(const T &t, const U &u, const string &hint) {
-  if (t != u) {
+void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+  if (!(t == u)) {
     ostringstream os;
-    os << "Assertion failed: " << t << " != " << u << " hint: " << hint;
+    os << "Assertion failed: " << t << " != " << u;
+    if (!hint.empty()) {
+      os << " hint: " << hint;
+    }
     throw runtime_error(os.str());
   }
 }
 
-inline void Assert(bool b, const string &hint) { AssertEqual(b, true, hint); }
+inline void Assert(bool b, const string& hint) { AssertEqual(b, true, hint); }
 
 class TestRunner {
-public:
+ public:
   template <class TestFunc>
-  void RunTest(TestFunc func, const string &test_name) {
+  void RunTest(TestFunc func, const string& test_name) {
     try {
       func();
       cerr << test_name << " OK" << endl;
-    } catch (runtime_error &e) {
+    } catch (exception& e) {
       ++fail_count;
       cerr << test_name << " fail: " << e.what() << endl;
+    } catch (...) {
+      ++fail_count;
+      cerr << "Unknown exception caught" << endl;
     }
   }
 
-  ~TestRunner();
+  ~TestRunner() {
+    if (fail_count > 0) {
+      cerr << fail_count << " unit tests failed. Terminate" << endl;
+      exit(1);
+    }
+  }
 
-private:
+ private:
   int fail_count = 0;
 };
+
+#define ASSERT_EQUAL(x, y)                                           \
+  {                                                                  \
+    ostringstream os;                                                \
+    os << #x << " != " << #y << ", " << __FILE__ << ":" << __LINE__; \
+    AssertEqual(x, y, os.str());                                     \
+  }
+
+#define ASSERT(x)                                             \
+  {                                                           \
+    ostringstream os;                                         \
+    os << #x << " is false, " << __FILE__ << ":" << __LINE__; \
+    Assert(x, os.str());                                      \
+  }
+
+#define RUN_TEST(tr, func) tr.RunTest(func, #func)
