17cde6a79f6b4c0962b7e22259739742603bdcac
Rustam Guseynov <guseynov.r.v@gmail.com>
2020-01-27 22:34:05 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "test_runner.h"
+
+void TestAll() {}
+
+int main() {
+  TestAll();
+
+  return 0;
+}
---
+++ test_runner.cpp
@@ -0,0 +1,9 @@
+#include "test_runner.h"
+#include <string>
+
+TestRunner::~TestRunner() {
+  if (fail_count > 0) {
+    cerr << fail_count << " unit tests failed. Terminate" << endl;
+    exit(1);
+  }
+}
