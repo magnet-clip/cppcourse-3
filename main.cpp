5ba7cf966f3a1b16a3615c624efb23fbea6eb5b0
Rustam <rustam.guseynov@refinitiv.com>
2020-01-28 15:26:13 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include "test_runner.h"
 
-void TestAll() {}
+#include <ostream>
+using namespace std;
+
+#define PRINT_VALUES(out, x, y)                                                \
+  do {                                                                         \
+    out << (x) << endl;                                                        \
+    out << (y) << endl;                                                        \
+  } while (0)
 
 int main() {
-  TestAll();
-
-  return 0;
+  // volatile int x = 2;
+  // ostringstream output;
+  // if (1 == x) {
+  //   PRINT_VALUES(output, 1 ? 2 : 3, x++);
+  //   if (2 == x)
+  //     PRINT_VALUES(output, []() { return 1; }(), x++);
+  //   else
+  //     PRINT_VALUES(output, 1, 2);
+  // }
+  TestRunner tr;
+  tr.RunTest(
+      [] {
+        ostringstream output;
+        PRINT_VALUES(output, 5, "red belt");
+        ASSERT_EQUAL(output.str(), "5\nred belt\n");
+      },
+      "PRINT_VALUES usage example");
 }
