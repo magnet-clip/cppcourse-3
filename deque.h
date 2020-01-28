05e8b5abd4d9d40a86619c23925363d2014387ef
Rustam <rustam.guseynov@refinitiv.com>
2020-01-28 18:24:54 +0300

    Testing

--- deque.h
+++ deque.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdexcept>
 #include <vector>
 
 template <typename T> class Deque {
@@ -7,10 +8,12 @@ public:
   Deque() {}
   bool Empty() const;
   size_t Size() const;
+
   T &operator[](size_t index);
   const T &operator[](size_t index) const;
-  const T &At(size_t index) const;
   T &At(size_t index);
+  const T &At(size_t index) const;
+
   const T &Front() const;
   T &Front();
   const T &Back() const;
@@ -23,7 +26,13 @@ private:
   inline bool IsInRange(size_t index) const;
   inline bool IsFront(size_t index) const;
 
-  std::vector<T> _front;
+  // input: index in total array, output: index in front
+  inline size_t FrontPosition(size_t index) const;
+
+  // input: index in total array, output: index in back
+  inline size_t BackPosition(size_t index) const;
+
+  std::vector<T> _front; // reversed
   std::vector<T> _back;
 };
 
@@ -32,7 +41,16 @@ template <typename T> inline bool Deque<T>::IsInRange(size_t index) const {
 }
 
 template <typename T> inline bool Deque<T>::IsFront(size_t index) const {
-  return index > _front.size();
+  return index < _front.size();
+}
+
+template <typename T>
+inline size_t Deque<T>::FrontPosition(size_t index) const {
+  return _front.size() - index - 1;
+}
+
+template <typename T> inline size_t Deque<T>::BackPosition(size_t index) const {
+  return index - _front.size();
 }
 
 template <typename T> bool Deque<T>::Empty() const {
@@ -41,4 +59,58 @@ template <typename T> bool Deque<T>::Empty() const {
 
 template <typename T> size_t Deque<T>::Size() const {
   return _front.size() + _back.size();
+}
+
+template <typename T> void Deque<T>::PushFront(const T &item) {
+  _front.push_back(item);
+}
+
+template <typename T> void Deque<T>::PushBack(const T &item) {
+  _back.push_back(item);
+}
+
+template <typename T> const T &Deque<T>::Front() const {
+  return _front.empty() ? *(_back.cbegin()) : _front.back();
+}
+
+template <typename T> T &Deque<T>::Front() {
+  return _front.empty() ? *(_back.begin()) : _front.back();
+}
+
+template <typename T> const T &Deque<T>::Back() const {
+  return _back.empty() ? *(_front.cbegin()) : _back.back();
+}
+
+template <typename T> T &Deque<T>::Back() {
+  return _back.empty() ? *(_front.begin()) : _back.back();
+}
+
+template <typename T> T &Deque<T>::operator[](size_t index) {
+  if (IsFront(index)) {
+    return _front[FrontPosition(index)];
+  } else {
+    return _back[BackPosition(index)];
+  }
+}
+
+template <typename T> const T &Deque<T>::operator[](size_t index) const {
+  if (IsFront(index)) {
+    return _front[FrontPosition(index)];
+  } else {
+    return _back[BackPosition(index)];
+  }
+}
+
+template <typename T> T &Deque<T>::At(size_t index) {
+  if (IsInRange(index))
+    return this->operator[](index);
+
+  throw std::out_of_range("");
+}
+
+template <typename T> const T &Deque<T>::At(size_t index) const {
+  if (IsInRange(index))
+    return this->operator[](index);
+
+  throw std::out_of_range("");
 }
--- main.cpp
+++ main.cpp
@@ -7,9 +7,125 @@ void TestEmpty() {
   ASSERT_EQUAL(dq.Size(), 0);
 }
 
+void TestBack() {
+  Deque<int> dq;
+  dq.PushBack(1);
+  ASSERT_EQUAL(dq.Back(), 1);
+  ASSERT_EQUAL(dq.Front(), 1);
+
+  dq.Back() = 3;
+  ASSERT_EQUAL(dq.Back(), 3);
+  ASSERT_EQUAL(dq.Front(), 3);
+}
+
+void TestFront() {
+  Deque<int> dq;
+  dq.PushFront(1);
+  ASSERT_EQUAL(dq.Back(), 1);
+  ASSERT_EQUAL(dq.Front(), 1);
+
+  dq.Front() = 3;
+  ASSERT_EQUAL(dq.Back(), 3);
+  ASSERT_EQUAL(dq.Front(), 3);
+
+  dq.PushFront(2);
+  ASSERT_EQUAL(dq.Back(), 3);
+  ASSERT_EQUAL(dq.Front(), 2);
+}
+
+void TestFrontAndBack() {
+  Deque<int> dq;
+  dq.PushFront(1);
+  ASSERT_EQUAL(dq.Back(), 1);
+  ASSERT_EQUAL(dq.Front(), 1);
+
+  dq.PushBack(3);
+  ASSERT_EQUAL(dq.Back(), 3);
+  ASSERT_EQUAL(dq.Front(), 1);
+
+  dq.PushFront(7);
+  ASSERT_EQUAL(dq.Back(), 3);
+  ASSERT_EQUAL(dq.Front(), 7);
+}
+
+void TestOrder() {
+  {
+    Deque<int> dq;
+    dq.PushFront(1);
+    ASSERT_EQUAL(dq[0], 1);
+    ASSERT_EQUAL(dq.At(0), 1);
+
+    dq.PushFront(2);
+    ASSERT_EQUAL(dq[0], 2);
+    ASSERT_EQUAL(dq[1], 1);
+    ASSERT_EQUAL(dq.At(0), 2);
+    ASSERT_EQUAL(dq.At(1), 1);
+
+    try {
+      dq.At(3);
+      ASSERT(false);
+    } catch (std::out_of_range &) {
+    }
+  }
+  {
+    Deque<int> dq;
+    dq.PushBack(1);
+    ASSERT_EQUAL(dq[0], 1);
+    ASSERT_EQUAL(dq.At(0), 1);
+
+    dq.PushBack(2);
+    ASSERT_EQUAL(dq[0], 1);
+    ASSERT_EQUAL(dq[1], 2);
+    ASSERT_EQUAL(dq.At(0), 1);
+    ASSERT_EQUAL(dq.At(1), 2);
+
+    try {
+      dq.At(3);
+      ASSERT(false);
+    } catch (std::out_of_range &) {
+    }
+  }
+  {
+    Deque<int> dq;
+    dq.PushBack(1); // 1
+    ASSERT_EQUAL(dq[0], 1);
+    ASSERT_EQUAL(dq.At(0), 1);
+
+    dq.PushBack(2); // 1 2
+    ASSERT_EQUAL(dq[0], 1);
+    ASSERT_EQUAL(dq[1], 2);
+    ASSERT_EQUAL(dq.At(0), 1);
+    ASSERT_EQUAL(dq.At(1), 2);
+    try {
+      dq.At(3);
+      ASSERT(false);
+    } catch (std::out_of_range &) {
+    }
+
+    dq.PushFront(10); // 10 1 2
+
+    ASSERT_EQUAL(dq[0], 10);
+    ASSERT_EQUAL(dq[1], 1);
+    ASSERT_EQUAL(dq[2], 2);
+
+    dq.PushFront(1); // 1 10 1 2
+    ASSERT_EQUAL(dq[0], 1);
+    ASSERT_EQUAL(dq[1], 10);
+    ASSERT_EQUAL(dq[2], 1);
+    ASSERT_EQUAL(dq[3], 2);
+
+    dq.PushBack(3); // 1 10 1 2 3
+    ASSERT_EQUAL(dq[4], 3);
+  }
+}
+
 void TestAll() {
   TestRunner tr;
   RUN_TEST(tr, TestEmpty);
+  RUN_TEST(tr, TestBack);
+  RUN_TEST(tr, TestFront);
+  RUN_TEST(tr, TestFrontAndBack);
+  RUN_TEST(tr, TestOrder);
 }
 
 int main() {
