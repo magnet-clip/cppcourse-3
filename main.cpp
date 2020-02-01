2075b2420808a5e50e2d59562bbc82110a9468a1
Rustam Guseynov <guseynov.r.v@gmail.com>
2020-02-01 18:22:22 +0300

    Some issue

--- main.cpp
+++ main.cpp
@@ -1,147 +1,192 @@
-// #include <cmath>
 #include <iomanip>
 #include <iostream>
 #include <map>
 #include <set>
 #include <utility>
 #include <vector>
-// #include "test_runner.h"
+#include "test_runner.h"
 
 using namespace std;
 
-class ReadingManager {
+template <auto N = 86400>
+class BookingManager {
  public:
-  ReadingManager() {}
+  BookingManager() : _time(0) {}
 
-  void Read(int user_id, int page_count) {
-    int old_page_count = 0;
+  void Book(int64_t curr_time, string hotel_name, uint32_t user_id,
+            int16_t room_count) {
+    auto prev_time = _time;
+    _time = curr_time;
 
-    // If user did exists, save his previous pages count
-    if (_users_pages.count(user_id)) {
-      old_page_count = _users_pages[user_id];
+    auto& hotel = _booking[hotel_name];
+
+    auto& [rooms, client] = hotel[curr_time];
+    rooms += room_count;
+    client.insert(user_id);
+
+    auto prev_since = hotel.upper_bound(prev_time - N);
+    auto curr_since = hotel.upper_bound(curr_time - N);
+    auto& [agg_rooms, agg_guests] = _aggregated[hotel_name];
+
+    for (auto& it = prev_since; it != curr_since; it++) {
+      const auto& [removed_rooms, removed_users] = it->second;
+      agg_rooms -= removed_rooms;
+      for (auto& removed_user_id : removed_users) {
+        if (agg_guests.count(user_id)) {
+          agg_guests[removed_user_id] -= 1;
+          if (agg_guests[removed_user_id] == 0) {
+            agg_guests.erase(removed_user_id);
+          }
+        }
+      }
     }
-    _users_pages[user_id] = page_count;
 
-    // If there are already users with same amount of pages
-    if (_number_of_users_with_pages.count(page_count)) {
-      // Increase their number
-      _number_of_users_with_pages[page_count] += 1;
-    } else {
-      // Otherwise indicate that now one does have such number of pages
-      _number_of_users_with_pages[page_count] = 1;
-    }
-    _pages_counts.insert(page_count);
-
-    _number_of_users_with_pages[old_page_count] -= 1;
-    if (_number_of_users_with_pages[old_page_count] == 0) {
-      _pages_counts.erase(old_page_count);
-    }
+    agg_guests[user_id] += 1;
+    agg_rooms += room_count;
   }
 
-  double Cheer(int user_id) const {
-    // 1) Check if he read anything
+  uint32_t Clients(string hotel_name) const {
+    // if (!_booking.count(hotel_name)) return 0;
+    // auto& hotel = _booking.at(hotel_name);
+    // auto since = hotel.upper_bound(_time - 86400);
+    // set<uint32_t> clients;
+    // for (auto& it = since; it != hotel.end(); it++) {
+    //   auto& t = it->second.second;
+    //   clients.insert(t.begin(), t.end());
+    // }
 
-    // If this user has not read anything
-    if (!_users_pages.count(user_id)) {
-      return 0;
-    }
+    // return clients.size();
+    if (!_aggregated.count(hotel_name)) return 0;
+    auto& hotel = _aggregated.at(hotel_name);
+    return hotel.second.size();
+  }
 
-    // If this user is the only user
-    if (_users_pages.size() == 1) {
-      return 1;
-    }
+  uint32_t Rooms(string hotel_name) const {
+    // if (!_booking.count(hotel_name)) return 0;
+    // auto& hotel = _booking.at(hotel_name);
+    // auto since = hotel.upper_bound(_time - 86400);
+    // uint32_t count = 0;
+    // for (auto& it = since; it != hotel.end(); it++) {
+    //   count += it->second.first;
+    // }
 
-    // 2) Find his pages count
-    auto pages_count = _users_pages.at(user_id);
+    // return count;
 
-    // 3) Find all pages counts less than his
-    auto finish = _pages_counts.lower_bound(pages_count);
-
-    // 4) Go thru all pages counts and sum up counts of users
-    auto total_users_with_pages_less_then_his = 0;
-    for (auto it = _pages_counts.begin(); it != finish; it++) {
-      total_users_with_pages_less_then_his +=
-          _number_of_users_with_pages.at(*it);
-    }
-
-    auto res =
-        total_users_with_pages_less_then_his * 1.0 / (_users_pages.size() - 1);
-    return res;
+    if (!_aggregated.count(hotel_name)) return 0;
+    auto& hotel = _aggregated.at(hotel_name);
+    return hotel.first;
   }
 
  private:
-  map<int, int> _users_pages;
-  map<int, int> _number_of_users_with_pages;
-  set<int> _pages_counts;
+  int64_t _time;
+  map<string, map<int64_t, pair<uint32_t, set<uint32_t>>>> _booking;
+  map<string, pair<uint32_t, map<uint32_t, uint32_t>>> _aggregated;
 };
 
-// template <typename T>
-// inline bool fpequal(T a, T b) {
-//   return fabs(a - b) < 1e-4;
-// }
+void TestSingle() {
+  BookingManager<100> manager;
+  ASSERT_EQUAL(manager.Clients("A"), 0);
+  ASSERT_EQUAL(manager.Rooms("A"), 0);
 
-// void TestVerySimple() {
-//   ReadingManager manager;
+  manager.Book(0, "A", 1, 1);  // at 0 user 1 books 1 room
+  ASSERT_EQUAL(manager.Clients("A"), 1);
+  ASSERT_EQUAL(manager.Rooms("A"), 1);
 
-//   manager.Read(1, 10);
-//   manager.Read(2, 20);
-//   manager.Read(3, 30);
+  manager.Book(10, "A", 1, 2);  // at 10 user 1 books 2 rooms
+  ASSERT_EQUAL(manager.Clients("A"), 1);
+  ASSERT_EQUAL(manager.Rooms("A"), 3);
 
-//   ASSERT(fpequal(manager.Cheer(1), 0.0));
-//   ASSERT(fpequal(manager.Cheer(2), 0.5));
-//   ASSERT(fpequal(manager.Cheer(3), 1.0));
-// }
+  manager.Book(20, "A", 2, 1);  // at 20 user 2 books 1 room
+  ASSERT_EQUAL(manager.Clients("A"), 2);
+  ASSERT_EQUAL(manager.Rooms("A"), 4);
 
-// void TestAsInSample() {
-//   ReadingManager manager;
+  manager.Book(100, "A", 3, 3);  // at 100 user 3 books 3 rooms
+  ASSERT_EQUAL(manager.Clients("A"), 3);
+  ASSERT_EQUAL(manager.Rooms("A"), 6);
 
-//   ASSERT(fpequal(manager.Cheer(5), 0.0));
+  manager.Book(105, "A", 3, 1);  // at 105 user 3 books 1 rooms
+  ASSERT_EQUAL(manager.Clients("A"), 3);
+  ASSERT_EQUAL(manager.Rooms("A"), 7);
 
-//   manager.Read(1, 10);
-//   ASSERT(fpequal(manager.Cheer(1), 1.0));
+  manager.Book(111, "A", 2, 4);  // at 111 user 2 books 4 rooms
+  ASSERT_EQUAL(manager.Rooms("A"), 9);
+  ASSERT_EQUAL(manager.Clients("A"), 2);
+}
 
-//   manager.Read(2, 5);
-//   manager.Read(3, 7);
-//   ASSERT(fpequal(manager.Cheer(2), 0.0));
-//   ASSERT(fpequal(manager.Cheer(3), 0.5));
+void TestSimple() {
+  BookingManager manager;
 
-//   manager.Read(3, 10);
-//   ASSERT(fpequal(manager.Cheer(3), 0.5));
+  // CLIENTS Marriott
+  ASSERT_EQUAL(manager.Clients("Marriott"), 0);
 
-//   manager.Read(3, 11);
-//   ASSERT(fpequal(manager.Cheer(3), 1.0));
-//   ASSERT(fpequal(manager.Cheer(1), 0.5));
-// }
+  // ROOMS Marriott
+  ASSERT_EQUAL(manager.Rooms("Marriott"), 0);
 
-// void TestAll() {
-//   TestRunner tr;
-//   RUN_TEST(tr, TestVerySimple);
-//   RUN_TEST(tr, TestAsInSample);
-// }
+  // BOOK 10 FourSeasons 1 2
+  manager.Book(10, "FourSeasons", 1, 2);
+
+  // BOOK 10 Marriott 1 1
+  manager.Book(10, "Marriott", 1, 1);
+
+  // BOOK 86409 FourSeasons 2 1
+  manager.Book(86409, "FourSeasons", 2, 1);
+
+  // CLIENTS FourSeasons
+  ASSERT_EQUAL(manager.Clients("FourSeasons"), 2);
+
+  // ROOMS FourSeasons
+  ASSERT_EQUAL(manager.Rooms("FourSeasons"), 3);
+
+  // CLIENTS Marriott
+  ASSERT_EQUAL(manager.Clients("Marriott"), 1);
+
+  // BOOK 86410 Marriott 2 10
+  manager.Book(86410, "Marriott", 2, 10);
+
+  // ROOMS FourSeasons
+  ASSERT_EQUAL(manager.Rooms("FourSeasons"), 1);
+
+  // ROOMS Marriott
+  ASSERT_EQUAL(manager.Rooms("Marriott"), 10);
+}
+
+void TestAll() {
+  TestRunner tr;
+  RUN_TEST(tr, TestSingle);
+  RUN_TEST(tr, TestSimple);
+}
 
 int main() {
-  // TestAll();
+  TestAll();
 
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  ReadingManager manager;
+  BookingManager manager;
 
   int query_count;
   cin >> query_count;
 
-  for (int query_id = 0; query_id < query_count; ++query_id) {
+  int idx = 0;
+  while (idx++ < query_count) {
     string query_type;
     cin >> query_type;
-    int user_id;
-    cin >> user_id;
 
-    if (query_type == "READ") {
-      int page_count;
-      cin >> page_count;
-      manager.Read(user_id, page_count);
-    } else if (query_type == "CHEER") {
-      cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+    if (query_type == "BOOK") {
+      int64_t time;
+      string hotel_name;
+      uint32_t user_id;
+      int16_t room_count;
+      cin >> time >> hotel_name >> user_id >> room_count;
+      manager.Book(time, hotel_name, user_id, room_count);
+    } else if (query_type == "CLIENTS") {
+      string hotel_name;
+      cin >> hotel_name;
+      cout << manager.Clients(hotel_name) << "\n";
+    } else if (query_type == "ROOMS") {
+      string hotel_name;
+      cin >> hotel_name;
+      cout << manager.Rooms(hotel_name) << "\n";
     }
   }
 
