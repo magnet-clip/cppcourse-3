aa30a7a510209a02266cf1417dbac3d3b140ab9c
Rustam Guseynov <guseynov.r.v@gmail.com>
2020-02-08 10:07:28 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <list>
 #include <string>
 #include "test_runner.h"
 using namespace std;
@@ -5,18 +6,60 @@ using namespace std;
 class Editor {
  public:
   // Реализуйте конструктор по умолчанию и объявленные методы
-  Editor();
-  void Left();
-  void Right();
-  void Insert(char token);
-  void Cut(size_t tokens = 1);
-  void Copy(size_t tokens = 1);
-  void Paste();
-  string GetText() const;
+  Editor() : _position(_text.begin()) {}
+
+  void Left() {
+    if (_position != _text.begin()) _position = prev(_position);
+  }
+
+  void Right() {
+    if (_position != _text.end()) _position = next(_position);
+  }
+
+  void Insert(char token) { _text.insert(_position, token); }
+
+  void Cut(size_t tokens = 1) {
+    _clipboard.clear();
+    if (tokens == 0) return;
+
+    size_t distance_till_end = distance(_position, _text.end());
+    size_t count = distance_till_end < tokens ? distance_till_end : tokens;
+
+    if (_position == _text.begin()) {
+      _clipboard.splice(_clipboard.begin(), _text, _position,
+                        next(_position, count));
+      _position = _text.begin();
+    } else {
+      auto step_back = prev(_position);
+      _clipboard.splice(_clipboard.begin(), _text, _position,
+                        next(_position, count));
+      _position = next(step_back);
+    }
+  }
+
+  void Copy(size_t tokens = 1) {
+    _clipboard.clear();
+
+    if (tokens == 0) return;
+    size_t distance_till_end = distance(_position, _text.end());
+    size_t count = distance_till_end < tokens ? distance_till_end : tokens;
+    _clipboard.insert(_clipboard.begin(), _position, next(_position, count));
+  }
+
+  void Paste() {
+    _text.insert(_position, _clipboard.begin(), _clipboard.end());
+  }
+
+  string GetText() const { return string(_text.begin(), _text.end()); }
+
+ private:
+  list<char> _text;
+  list<char>::iterator _position;
+  list<char> _clipboard;
 };
 
 void TypeText(Editor& editor, const string& text) {
-  for(char c : text) {
+  for (char c : text) {
     editor.Insert(c);
   }
 }
@@ -28,11 +71,11 @@ void TestEditing() {
     const size_t text_len = 12;
     const size_t first_part_len = 7;
     TypeText(editor, "hello, world");
-    for(size_t i = 0; i < text_len; ++i) {
+    for (size_t i = 0; i < text_len; ++i) {
       editor.Left();
     }
     editor.Cut(first_part_len);
-    for(size_t i = 0; i < text_len - first_part_len; ++i) {
+    for (size_t i = 0; i < text_len - first_part_len; ++i) {
       editor.Right();
     }
     TypeText(editor, ", ");
@@ -40,12 +83,13 @@ void TestEditing() {
     editor.Left();
     editor.Left();
     editor.Cut(3);
-    
+
     ASSERT_EQUAL(editor.GetText(), "world, hello");
   }
+
   {
     Editor editor;
-    
+
     TypeText(editor, "misprnit");
     editor.Left();
     editor.Left();
@@ -53,7 +97,7 @@ void TestEditing() {
     editor.Cut(1);
     editor.Right();
     editor.Paste();
-    
+
     ASSERT_EQUAL(editor.GetText(), "misprint");
   }
 }
@@ -62,18 +106,18 @@ void TestReverse() {
   Editor editor;
 
   const string text = "esreveR";
-  for(char c : text) {
+  for (char c : text) {
     editor.Insert(c);
     editor.Left();
   }
-  
+
   ASSERT_EQUAL(editor.GetText(), "Reverse");
 }
 
 void TestNoText() {
   Editor editor;
   ASSERT_EQUAL(editor.GetText(), "");
-  
+
   editor.Left();
   editor.Left();
   editor.Right();
@@ -81,7 +125,7 @@ void TestNoText() {
   editor.Copy(0);
   editor.Cut(0);
   editor.Paste();
-  
+
   ASSERT_EQUAL(editor.GetText(), "");
 }
 
@@ -100,7 +144,7 @@ void TestEmptyBuffer() {
   editor.Left();
   editor.Cut(0);
   editor.Paste();
-  
+
   ASSERT_EQUAL(editor.GetText(), "example");
 }
 
