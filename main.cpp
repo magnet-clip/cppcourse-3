52c99861ba250d9762f07cdc1abf6ed630d6ef9b
Rustam <rustam.guseynov@refinitiv.com>
2020-02-10 16:40:33 +0300

    Almost

--- main.cpp
+++ main.cpp
@@ -9,50 +9,86 @@
 
 using namespace std;
 
-template <typename T>
-class PriorityCollection {
-public:
-  using Id = /* тип, используемый для идентификаторов */;
+template <typename T, typename Id> struct rec {
+  T value;
+  Id id;
+  int priority;
 
+  rec(T &&v, Id i, int p) : value(move(v)), id(i), priority(p) {}
+};
+
+template <typename T, typename Id>
+bool operator<(const rec<T, Id> &a, const rec<T, Id> &b) {
+  return tie(a.priority, a.id) > tie(b.priority, b.id);
+}
+
+template <typename T> class PriorityCollection {
+public:
+  using Id = int;
+  Id _last = 0;
+
+private:
+  using Item = rec<T, Id>;
+  using Pool = set<Item>;
+  using PoolIterator = typename set<Item>::iterator;
+
+  Pool _pool;
+  map<Id, PoolIterator> _references;
+
+public:
   // Добавить объект с нулевым приоритетом
   // с помощью перемещения и вернуть его идентификатор
-  Id Add(T object);
+  Id Add(T object) {
+    auto new_id = _last++;
+    auto inserted = _pool.insert({move(object), new_id, 0});
+    _references[new_id] = inserted.first;
+    return new_id;
+  }
 
   // Добавить все элементы диапазона [range_begin, range_end)
   // с помощью перемещения, записав выданные им идентификаторы
   // в диапазон [ids_begin, ...)
   template <typename ObjInputIt, typename IdOutputIt>
-  void Add(ObjInputIt range_begin, ObjInputIt range_end,
-           IdOutputIt ids_begin);
+  void Add(ObjInputIt range_begin, ObjInputIt range_end, IdOutputIt ids_begin) {
+    for (auto it = range_begin; it != range_end; it++) {
+      *ids_begin++ = Add(*it);
+    }
+  }
 
   // Определить, принадлежит ли идентификатор какому-либо
   // хранящемуся в контейнере объекту
-  bool IsValid(Id id) const;
+  bool IsValid(Id id) const { return _references.count(id); }
 
   // Получить объект по идентификатору
-  const T& Get(Id id) const;
+  const T &Get(Id id) const { return (_references.at(id))->first; }
 
   // Увеличить приоритет объекта на 1
-  void Promote(Id id);
+  void Promote(Id id) {
+    auto iterator = _references[id];
+    auto handle = _pool.extract(iterator);
+    handle.value().priority++;
+    _pool.insert(move(handle));
+  }
 
   // Получить объект с максимальным приоритетом и его приоритет
-  pair<const T&, int> GetMax() const;
+  pair<const T &, int> GetMax() const;
 
   // Аналогично GetMax, но удаляет элемент из контейнера
-  pair<T, int> PopMax();
-
-private:
-  // Приватные поля и методы
+  pair<T, int> PopMax() {
+    auto item = _pool.begin();
+    auto handle = _pool.extract(item);
+    _references.erase(handle.value().id);
+    return {move(handle.value().value), handle.value().priority};
+  }
 };
-
 
 class StringNonCopyable : public string {
 public:
-  using string::string;  // Позволяет использовать конструкторы строки
-  StringNonCopyable(const StringNonCopyable&) = delete;
-  StringNonCopyable(StringNonCopyable&&) = default;
-  StringNonCopyable& operator=(const StringNonCopyable&) = delete;
-  StringNonCopyable& operator=(StringNonCopyable&&) = default;
+  using string::string; // Позволяет использовать конструкторы строки
+  StringNonCopyable(const StringNonCopyable &) = delete;
+  StringNonCopyable(StringNonCopyable &&) = default;
+  StringNonCopyable &operator=(const StringNonCopyable &) = delete;
+  StringNonCopyable &operator=(StringNonCopyable &&) = default;
 };
 
 void TestNoCopy() {
