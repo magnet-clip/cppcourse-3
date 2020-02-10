abf3b02dad62c3445d051172ab383a041ccb8808
Rustam <rustam.guseynov@refinitiv.com>
2020-02-10 09:36:50 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,84 +1,90 @@
 #include "test_runner.h"
 #include <algorithm>
+#include <iostream>
+#include <iterator>
 #include <memory>
+#include <set>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
-class NonCopyableInt {
+template <typename T>
+class PriorityCollection {
 public:
-  int value;
-  NonCopyableInt(const NonCopyableInt &) = delete;
-  NonCopyableInt &operator=(const NonCopyableInt &) = delete;
-  NonCopyableInt(NonCopyableInt &&) = default;
-  NonCopyableInt &operator=(NonCopyableInt &&) = default;
+  using Id = /* тип, используемый для идентификаторов */;
+
+  // Добавить объект с нулевым приоритетом
+  // с помощью перемещения и вернуть его идентификатор
+  Id Add(T object);
+
+  // Добавить все элементы диапазона [range_begin, range_end)
+  // с помощью перемещения, записав выданные им идентификаторы
+  // в диапазон [ids_begin, ...)
+  template <typename ObjInputIt, typename IdOutputIt>
+  void Add(ObjInputIt range_begin, ObjInputIt range_end,
+           IdOutputIt ids_begin);
+
+  // Определить, принадлежит ли идентификатор какому-либо
+  // хранящемуся в контейнере объекту
+  bool IsValid(Id id) const;
+
+  // Получить объект по идентификатору
+  const T& Get(Id id) const;
+
+  // Увеличить приоритет объекта на 1
+  void Promote(Id id);
+
+  // Получить объект с максимальным приоритетом и его приоритет
+  pair<const T&, int> GetMax() const;
+
+  // Аналогично GetMax, но удаляет элемент из контейнера
+  pair<T, int> PopMax();
+
+private:
+  // Приватные поля и методы
 };
 
-bool operator<(NonCopyableInt a, NonCopyableInt b) { return a.value < b.value; }
 
-template <typename RandomIt>
-void MergeSort(RandomIt range_begin, RandomIt range_end) {
-  // 1) Если диапазон содержит меньше 2 элементов, выйти из функции.
-  size_t count = distance(range_begin, range_end);
-  if (count < 2) {
-    return;
+class StringNonCopyable : public string {
+public:
+  using string::string;  // Позволяет использовать конструкторы строки
+  StringNonCopyable(const StringNonCopyable&) = delete;
+  StringNonCopyable(StringNonCopyable&&) = default;
+  StringNonCopyable& operator=(const StringNonCopyable&) = delete;
+  StringNonCopyable& operator=(StringNonCopyable&&) = default;
+};
+
+void TestNoCopy() {
+  PriorityCollection<StringNonCopyable> strings;
+  const auto white_id = strings.Add("white");
+  const auto yellow_id = strings.Add("yellow");
+  const auto red_id = strings.Add("red");
+
+  strings.Promote(yellow_id);
+  for (int i = 0; i < 2; ++i) {
+    strings.Promote(red_id);
   }
-
-  // 2) Создать вектор, содержащий все элементы текущего диапазона.
-  vector<typename RandomIt::value_type> temp;
-  temp.reserve(count);
-  move(range_begin, range_end, back_inserter(temp));
-
-  // 3) Разбить вектор на три равные части. (В этой задаче гарантируется, что
-  // длина передаваемого диапазона является степенью тройки, так что вектор
-  // всегда можно разбить на три равные части.)
-
-  // 4) Вызвать функцию MergeSort от каждой части вектора.
-  MergeSort(temp.begin(), temp.begin() + count / 3);
-  MergeSort(temp.begin() + count / 3, temp.begin() + 2 * count / 3);
-  MergeSort(temp.begin() + 2 * count / 3, temp.end());
-
-  // 5) Слить первые две трети вектора с помощью алгоритма merge, сохранив
-  // результат во временный вектор с помощью back_inserter.
-  vector<typename RandomIt::value_type> temp2;
-  temp.reserve(count);
-
-  merge(make_move_iterator(temp.begin()),
-        make_move_iterator(temp.begin() + count / 3),
-        make_move_iterator(temp.begin() + count / 3),
-        make_move_iterator(temp.begin() + 2 * count / 3), back_inserter(temp2));
-
-  // 6) Слить временный вектор из предыдущего пункта с последней третью вектора
-  // из п. 2, записав полученный отсортированный диапазон вместо исходного.
-
-  merge(make_move_iterator(temp2.begin()), make_move_iterator(temp2.end()),
-        make_move_iterator(temp.begin() + 2 * count / 3),
-        make_move_iterator(temp.end()), range_begin);
-}
-
-void TestIntVector() {
-  vector<int> numbers = {6, 1, 3, 9, 1, 9, 8, 12, 1};
-  MergeSort(begin(numbers), end(numbers));
-  ASSERT(is_sorted(begin(numbers), end(numbers)));
-}
-
-void TestNonCopyIntVector() {
-  vector<NonCopyableInt> numbers;
-  numbers.push_back({6});
-  numbers.push_back({1});
-  numbers.push_back({3});
-  numbers.push_back({9});
-  numbers.push_back({1});
-  numbers.push_back({9});
-  numbers.push_back({8});
-  numbers.push_back({12});
-  numbers.push_back({1});
-  MergeSort(begin(numbers), end(numbers));
+  strings.Promote(yellow_id);
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "red");
+    ASSERT_EQUAL(item.second, 2);
+  }
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "yellow");
+    ASSERT_EQUAL(item.second, 2);
+  }
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "white");
+    ASSERT_EQUAL(item.second, 0);
+  }
 }
 
 int main() {
   TestRunner tr;
-  RUN_TEST(tr, TestIntVector);
-  RUN_TEST(tr, TestNonCopyIntVector);
+  RUN_TEST(tr, TestNoCopy);
   return 0;
 }
