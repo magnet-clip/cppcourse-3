c378d2bdf041af7d18312faf5ca51f3c48291ae7
Rustam <rustam.guseynov@refinitiv.com>
2020-02-10 09:05:04 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -14,10 +14,46 @@ public:
   NonCopyableInt &operator=(NonCopyableInt &&) = default;
 };
 
+bool operator<(NonCopyableInt a, NonCopyableInt b) { return a.value < b.value; }
+
 template <typename RandomIt>
 void MergeSort(RandomIt range_begin, RandomIt range_end) {
-  // Напишите реализацию функции,
-  // не копируя сортируемые элементы
+  // 1) Если диапазон содержит меньше 2 элементов, выйти из функции.
+  size_t count = distance(range_begin, range_end);
+  if (count < 2) {
+    return;
+  }
+
+  // 2) Создать вектор, содержащий все элементы текущего диапазона.
+  vector<typename RandomIt::value_type> temp;
+  temp.reserve(count);
+  move(range_begin, range_end, back_inserter(temp));
+
+  // 3) Разбить вектор на три равные части. (В этой задаче гарантируется, что
+  // длина передаваемого диапазона является степенью тройки, так что вектор
+  // всегда можно разбить на три равные части.)
+
+  // 4) Вызвать функцию MergeSort от каждой части вектора.
+  MergeSort(temp.begin(), temp.begin() + count / 3);
+  MergeSort(temp.begin() + count / 3, temp.begin() + 2 * count / 3);
+  MergeSort(temp.begin() + 2 * count / 3, temp.end());
+
+  // 5) Слить первые две трети вектора с помощью алгоритма merge, сохранив
+  // результат во временный вектор с помощью back_inserter.
+  vector<typename RandomIt::value_type> temp2;
+  temp.reserve(count);
+
+  merge(make_move_iterator(temp.begin()),
+        make_move_iterator(temp.begin() + count / 3),
+        make_move_iterator(temp.begin() + count / 3),
+        make_move_iterator(temp.begin() + 2 * count / 3), back_inserter(temp2));
+
+  // 6) Слить временный вектор из предыдущего пункта с последней третью вектора
+  // из п. 2, записав полученный отсортированный диапазон вместо исходного.
+
+  merge(make_move_iterator(temp2.begin()), make_move_iterator(temp2.end()),
+        make_move_iterator(temp.begin() + 2 * count / 3),
+        make_move_iterator(temp.end()), range_begin);
 }
 
 void TestIntVector() {
@@ -43,5 +79,6 @@ void TestNonCopyIntVector() {
 int main() {
   TestRunner tr;
   RUN_TEST(tr, TestIntVector);
+  RUN_TEST(tr, TestNonCopyIntVector);
   return 0;
 }
