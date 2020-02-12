4ae69a8af3ee873e144aac2e72e0d30533ee9601
Rustam Guseynov <guseynov.r.v@gmail.com>
2020-02-12 22:03:26 +0300

    Unordered map seems to be tiny bit faster

--- search_server.h
+++ search_server.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "utils.h"
 
 #include <istream>
 #include <ostream>
@@ -10,26 +11,26 @@
 using namespace std;
 
 class InvertedIndex {
-public:
-  void Add(const string& document);
-  list<size_t> Lookup(const string& word) const;
+ public:
+  void Add(const string &document);
+  list<size_t> Lookup(const string &word) const;
 
-  const string& GetDocument(size_t id) const {
+  const string &GetDocument(size_t id) const {
     return docs[id];
   }
 
-private:
-  map<string, list<size_t>> index;
+ private:
+  Map<string, list<size_t>> index;
   vector<string> docs;
 };
 
 class SearchServer {
-public:
+ public:
   SearchServer() = default;
-  explicit SearchServer(istream& document_input);
-  void UpdateDocumentBase(istream& document_input);
-  void AddQueriesStream(istream& query_input, ostream& search_results_output);
+  explicit SearchServer(istream &document_input);
+  void UpdateDocumentBase(istream &document_input);
+  void AddQueriesStream(istream &query_input, ostream &search_results_output);
 
-private:
+ private:
   InvertedIndex index;
 };
---
+++ utils.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <unordered_map>
+#include <map>
+
+template<typename K, typename V>
+using Map = std::unordered_map<K,V>;
